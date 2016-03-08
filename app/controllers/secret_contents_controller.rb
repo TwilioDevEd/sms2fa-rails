c80611f957e2a33c36b50bd6122719c236046683
Ronualdo Maciel <user.email>
2016-03-08 18:22:21 -0300

    Code style fixes
    Remove some trailing whitespaces

--- app/controllers/secret_contents_controller.rb
+++ app/controllers/secret_contents_controller.rb
@@ -1,6 +1,6 @@
 class SecretContentsController < ApplicationController
   before_action :authenticate!
-  
+
   def show
     render 'users/top_secret'
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,10 @@
 class SessionsController < ApplicationController
-
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       session[:authenticated] = false
-      render "users/confirmation"
+      render 'users/confirmation'
     else
       render nothing: true
     end
