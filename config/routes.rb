b8f33b2f845b26b935e3fc180c6cec9e438d8d87
Victor Shyba <victor.shyba@gmail.com>
2016-03-09 18:34:46 -0300

    Adds a logout route
    GET on /logout will ease session data and redirect to login page.

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   #   resources :products
   resources :users, only: [:new, :create]
   get 'secret_contents', to: 'secret_contents#show'
+  get '/logout', to: 'sessions#destroy'
   post '/login', to: 'sessions#create'
   post '/verify-code', to: 'users#confirm'
   get '/signin', to: 'sessions#new'
