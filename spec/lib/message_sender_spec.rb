925ebcabdd6eeab211665898d7ce14edc7be784e
Agustin Camino <agustin.camino@gmail.com>
2016-03-10 17:07:35 -0500

    Remove rails_helper from message sender spec

--- spec/lib/message_sender_spec.rb
+++ spec/lib/message_sender_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
 require 'vcr'
-require 'vcr_helper'
-require 'message_sender'
+require 'twilio-ruby'
+require_relative '../../lib/message_sender'
+require_relative '../vcr_helper'
 
 describe MessageSender do
-  describe '#send_code' do
+  describe '.send_code' do
     it 'sends a code over SMS to a provided number' do
       VCR.use_cassette('send_sms_to_user') do
         success = described_class.send_code('+12025550116', '123456')
